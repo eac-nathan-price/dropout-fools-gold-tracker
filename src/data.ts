3d8d28841c9efac7df66a932cf1c5e942948b4a5
Nathan Price-Lee <nathan@edassess.net>
2025-07-16 14:07:37 -0700

    46hrs

--- src/data.ts
+++ src/data.ts
@@ -54,7 +54,8 @@ export const sampleTimes: Date[] = [
     new Date("2025-07-15T16:00:00-07:00"),
     new Date("2025-07-15T20:00:00-07:00"),
     new Date("2025-07-16T06:00:00-07:00"),
-    new Date("2025-07-16T10:00:00-07:00")
+    new Date("2025-07-16T10:00:00-07:00"),
+    new Date("2025-07-16T14:00:00-07:00")
 ];
 
 export const videoData: Video[] = [
@@ -63,90 +64,90 @@ export const videoData: Video[] = [
         title: "Peel Robalino",
         link: "https://www.youtube.com/shorts/gMpx4A2lRTE",
         producers: ["trapp"], // Trapp only
-        youtubeViews: [0, 50000, 78000, 113000, 130000, 151000, 158000],
-        tiktokViews: [0, 61700, 84300, 108800, 125600, 148000, 154900],
-        instagramViews: [0, 148000, 296000, 509000, 537000, 570000, 581000]
+        youtubeViews: [0, 50000, 78000, 113000, 130000, 151000, 158000, 166000],
+        tiktokViews: [0, 61700, 84300, 108800, 125600, 148000, 154900, 162000],
+        instagramViews: [0, 148000, 296000, 509000, 537000, 570000, 581000, 591000]
     },
     {
         id: "video_002",
         title: "Anna's King for a Day",
         link: "https://www.youtube.com/shorts/UjHk90dxX20",
         producers: ["jordan"], // Jordan only
-        youtubeViews: [0, 70000, 118000, 218000, 261000, 312000, 328000],
-        tiktokViews: [0, 127400, 255500, 354800, 391600, 435400, 456400],
-        instagramViews: [0, 114000, 229000, 396000, 428000, 466000, 482000]
+        youtubeViews: [0, 70000, 118000, 218000, 261000, 312000, 328000, 356000],
+        tiktokViews: [0, 127400, 255500, 354800, 391600, 435400, 456400, 473500],
+        instagramViews: [0, 114000, 229000, 396000, 428000, 466000, 482000, 505000]
     },
     {
         id: "video_003",
         title: "Katie's D20 on a Bus",
         link: "https://www.youtube.com/shorts/5feqZBLXrMg",
         producers: ["rekha"], // Rekha only
-        youtubeViews: [0, 201000, 403000, 636000, 709000, 804000, 841000],
-        tiktokViews: [0, 338900, 545200, 726200, 797300, 868000, 894400],
-        instagramViews: [0, 229000, 458000, 797000, 873000, 995000, 1000000]
+        youtubeViews: [0, 201000, 403000, 636000, 709000, 804000, 841000, 878000],
+        tiktokViews: [0, 338900, 545200, 726200, 797300, 868000, 894400, 925200],
+        instagramViews: [0, 229000, 458000, 797000, 873000, 995000, 1000000, 1000000]
     },
     {
         id: "video_004",
         title: "Erika's Haircut",
         link: "https://www.youtube.com/shorts/wQVIfuNIc9I",
         producers: ["jordan"], // Jordan only
-        youtubeViews: [0, 189000, 364700, 492000, 555000, 626000, 650000],
-        tiktokViews: [0, 103700, 145800, 181500, 199000, 219600, 227600],
-        instagramViews: [0, 113000, 226000, 390000, 415000, 442000, 451000]
+        youtubeViews: [0, 189000, 364700, 492000, 555000, 626000, 650000, 676000],
+        tiktokViews: [0, 103700, 145800, 181500, 199000, 219600, 227600, 235900],
+        instagramViews: [0, 113000, 226000, 390000, 415000, 442000, 451000, 460000]
     },
     {
         id: "video_005",
         title: "Sephie's Sexy Car Wash",
         link: "https://www.youtube.com/shorts/HD5pyGbO_Is",
         producers: ["rekha", "jordan"], // Rekha and Jordan (50/50)
-        youtubeViews: [0, 66000, 102000, 145000, 165000, 194000, 201000],
-        tiktokViews: [0, 73700, 97600, 122900, 136300, 150300, 155600],
-        instagramViews: [0, 162000, 323000, 553000, 585000, 625000, 638000]
+        youtubeViews: [0, 66000, 102000, 145000, 165000, 194000, 201000, 210000],
+        tiktokViews: [0, 73700, 97600, 122900, 136300, 150300, 155600, 160900],
+        instagramViews: [0, 162000, 323000, 553000, 585000, 625000, 638000, 651000]
     },
     {
         id: "video_006",
         title: "Grant's Crack",
         link: "https://www.youtube.com/shorts/1lnl0jYln8s",
         producers: ["trapp"], // Trapp only
-        youtubeViews: [0, 53000, 79000, 111000, 124000, 140000, 145000],
-        tiktokViews: [0, 64100, 97900, 136200, 155300, 179400, 190300],
-        instagramViews: [0, 86000, 172000, 296000, 314000, 331000, 337000]
+        youtubeViews: [0, 53000, 79000, 111000, 124000, 140000, 145000, 151000],
+        tiktokViews: [0, 64100, 97900, 136200, 155300, 179400, 190300, 201100],
+        instagramViews: [0, 86000, 172000, 296000, 314000, 331000, 337000, 342000]
     },
     {
         id: "video_007",
         title: "Jonny's Human Puppy Bowl",
         link: "https://www.youtube.com/shorts/aagwlycxv_k",
         producers: ["trapp", "rekha"], // Trapp and Rekha (50/50)
-        youtubeViews: [0, 46000, 71000, 104000, 117000, 134000, 139000],
-        tiktokViews: [0, 89600, 130700, 165400, 178400, 192500, 197300],
-        instagramViews: [0, 68000, 136000, 234000, 251000, 266000, 271000]
+        youtubeViews: [0, 46000, 71000, 104000, 117000, 134000, 139000, 145000],
+        tiktokViews: [0, 89600, 130700, 165400, 178400, 192500, 197300, 201900],
+        instagramViews: [0, 68000, 136000, 234000, 251000, 266000, 271000, 277000]
     },
     {
         id: "video_008",
         title: "Lily & Izzy's Milk Taste Test",
         link: "https://www.youtube.com/shorts/nfwmaVlp_hY",
         producers: ["jordan"], // Jordan only
-        youtubeViews: [0, 66000, 107000, 158000, 181000, 208000, 217000],
-        tiktokViews: [0, 292000, 422800, 570100, 636700, 696600, 725500],
-        instagramViews: [0, 420000, 840000, 1500000, 1700000, 2000000, 2100000]
+        youtubeViews: [0, 66000, 107000, 158000, 181000, 208000, 217000, 226000],
+        tiktokViews: [0, 292000, 422800, 570100, 636700, 696600, 725500, 765700],
+        instagramViews: [0, 420000, 840000, 1500000, 1700000, 2000000, 2100000, 2200000]
     },
     {
         id: "video_009",
         title: "Izzy's Buttholes",
         link: "https://www.youtube.com/shorts/Wm8SMsmWCts",
         producers: ["trapp", "rekha"], // Trapp and Rekha (50/50)
-        youtubeViews: [0, 186000, 372000, 654000, 769000, 912000, 957000],
-        tiktokViews: [0, 90300, 125200, 167500, 189500, 206700, 210800],
-        instagramViews: [0, 114000, 227000, 392000, 424000, 453000, 465000]
+        youtubeViews: [0, 186000, 372000, 654000, 769000, 912000, 957000, 1000000],
+        tiktokViews: [0, 90300, 125200, 167500, 189500, 206700, 210800, 214700],
+        instagramViews: [0, 114000, 227000, 392000, 424000, 453000, 465000, 475000]
     },
     {
         id: "video_010",
         title: "Vic's Brennan's Exit Video",
         link: "https://www.youtube.com/shorts/oO4kgmYivoQ",
         producers: ["trapp", "rekha", "jordan", "sam"], // All producers (25% each)
-        youtubeViews: [0, 415000, 825000, 1300000, 1600000, 1800000, 1900000],
-        tiktokViews: [0, 881200, 1500000, 1900000, 2100000, 2200000, 2300000],
-        instagramViews: [0, 540000, 1080000, 1800000, 2000000, 2200000, 2200000]
+        youtubeViews: [0, 415000, 825000, 1300000, 1600000, 1800000, 1900000, 1900000],
+        tiktokViews: [0, 881200, 1500000, 1900000, 2100000, 2200000, 2300000, 2300000],
+        instagramViews: [0, 540000, 1080000, 1800000, 2000000, 2200000, 2200000, 2300000]
     }
 ];
 
